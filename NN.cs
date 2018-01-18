c95131f9174dbe3bfa0ab67ca3b20f396decbb84
Amin Babadi <amin.babadi@yahoo.com>
2018-01-18 20:54:46 +0200

    Added NN & RL scripts.

---
+++ NN.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class NN
+{
+    //user defineable variables
+    int numEpochs;
+    //number of inputs - this includes the input bias
+    int numInputs = 0;
+    //number of hidden units
+    int numHiddens;
+    //number of output units
+    int numOutputs;
+    //Size of experience replay memory!
+    int sizeOfExperienceReplayMemory;
+    //learning rate
+    float LR;
+
+    int batchSize;
+
+    //training data
+    List<RL.Transition> transitions, batchTransitions;
+
+    //the outputs of the neurons
+    float[] hiddenVal;
+    float[] outputVal;
+
+    //the errors
+    float[] hiddenDelta;
+    float[] outputDelta;
+
+    //the weights
+    float[,] weightsIH;
+    float[,] weightsHO;
+    
+    public NN(int inputs, int hiddens, int outputs)
+    {
+        transitions = new List<RL.Transition>();
+        batchTransitions = new List<RL.Transition>();
+        numInputs = inputs;
+        numHiddens = hiddens;
+        numOutputs = outputs;
+        numEpochs = 50;
+        sizeOfExperienceReplayMemory = 500;
+        LR = 0.01f;
+        batchSize = 25;
+        InitWeights();
+    }
+
+    void InitWeights()
+    {
+        hiddenVal = new float[numHiddens];
+        outputVal = new float[numOutputs];
+        hiddenDelta = new float[numHiddens];
+        outputDelta = new float[numOutputs];
+        weightsIH = new float[numInputs, numHiddens];
+        weightsHO = new float[numHiddens + 1, numOutputs];
+        for (int j = 0; j < numHiddens; j++)
+        {
+            for (int i = 0; i < numInputs; i++)
+                weightsIH[i, j] = (Random.Range(0.0f, 1.0f) - 0.5f) / 10;
+            for (int i = 0; i < numOutputs; i++)
+                weightsHO[j, i] = (Random.Range(0.0f, 1.0f) - 0.5f) / 10;
+        }
+        for (int i = 0; i < numOutputs; i++)
+            weightsHO[numHiddens, i] = (Random.Range(0.0f, 1.0f) - 0.5f) / 10;
+    }
+
+    float sigmoid(float x)
+    {
+        return 1.0f / (1 + (float)Mathf.Exp((float)-x));
+    }
+
+    public List<float> calcNet(float[] input)
+    {
+        //calculate the outputs of the hidden neurons
+        for (int i = 0; i < numHiddens; i++)
+        {
+            hiddenVal[i] = 0;
+            for (int j = 0; j < numInputs; j++)
+                hiddenVal[i] += input[j] * weightsIH[j, i];
+            hiddenVal[i] = sigmoid(hiddenVal[i]);
+        }
+        //calculate the output of the network
+        List<float> outPred = new List<float>();
+        for (int j = 0; j < numOutputs; j++)
+        {
+            outputVal[j] = 0;
+            for (int i = 0; i < numHiddens; i++)
+                outputVal[j] += hiddenVal[i] * weightsHO[i, j];
+            outputVal[j] += 1 * weightsHO[numHiddens, j];
+            outputVal[j] = sigmoid(outputVal[j]);
+            outPred.Add(outputVal[j]);
+        }
+        return outPred;
+    }
+
+    public void Train(RL.Transition newTransition)
+    {
+        while (transitions.Count >= sizeOfExperienceReplayMemory)
+            transitions.RemoveAt(0);
+        transitions.Add(newTransition);
+        TrainNetwork();
+    }
+
+    bool SampleBatch()
+    {
+        batchTransitions.Clear();
+        if (transitions.Count < 2 * batchSize)
+            return false;
+        for (int i = 0; i < Mathf.Min(batchSize, transitions.Count); i++)
+            batchTransitions.Add(transitions[Random.Range(0, transitions.Count)]);
+        return true;
+    }
+
+    void TrainNetwork()
+    {
+        if (!SampleBatch())
+            return;
+        for (int e = 0; e < numEpochs; e++)
+        {
+            for (int t = 0; t < batchTransitions.Count; t++)
+            {
+                float target = batchTransitions[t].reward;
+                float output = calcNet(batchTransitions[t].state)[batchTransitions[t].action];
+                //calculate the error
+                outputDelta[batchTransitions[t].action] = (target - output) * output * (1 - output);
+                //change network weights
+                WeightChangesHO(batchTransitions[t].action, output);
+                WeightChangesIH(batchTransitions[t], output);
+            }
+        }
+    }
+
+    //adjust the weights hidden-output
+    void WeightChangesHO(int action, float outPred)
+    {
+        for (int k = 0; k <= numHiddens; k++)
+        {
+            float gradient = outputDelta[action] * 1; //This is for bias weight
+            if (k < numHiddens)
+                gradient = outputDelta[action] * hiddenVal[k];
+            weightsHO[k, action] += LR * gradient;
+        }
+    }
+
+    //adjust the weights input-hidden
+    void WeightChangesIH(RL.Transition curTransition, float outPred)
+    {
+        for (int h = 0; h < numHiddens; h++)
+        {
+            hiddenDelta[h] = weightsHO[h, curTransition.action] * outputDelta[curTransition.action];
+            hiddenDelta[h] *= hiddenVal[h] * (1 - hiddenVal[h]); //Derivative for sigmoid function
+            for (int i = 0; i < numInputs; i++)
+            {
+                float gradient = hiddenDelta[h] * curTransition.state[i];
+                weightsIH[i, h] += LR * gradient;
+            }
+        }
+    }
+
+    public void Save(StreamWriter writer)
+    {
+        writer.WriteLine(numEpochs);
+        writer.WriteLine(numInputs);
+        writer.WriteLine(numHiddens);
+        writer.WriteLine(numOutputs);
+        writer.WriteLine(sizeOfExperienceReplayMemory);
+        writer.WriteLine(LR);
+        writer.WriteLine(LR);
+        for (int i = 0; i < numInputs; i++)
+            for (int h = 0; h < numHiddens; h++)
+            {
+                writer.WriteLine(weightsIH[i, h]);
+            }
+        for (int h = 0; h <= numHiddens; h++)
+            for (int o = 0; o < numOutputs; o++)
+            {
+                writer.WriteLine(weightsHO[h, o]);
+            }
+    }
+
+    public void Load(StreamReader reader)
+    {
+        try
+        {
+            numEpochs = int.Parse(reader.ReadLine());
+            numInputs = int.Parse(reader.ReadLine());
+            numHiddens = int.Parse(reader.ReadLine());
+            numOutputs = int.Parse(reader.ReadLine());
+            InitWeights();
+            sizeOfExperienceReplayMemory = int.Parse(reader.ReadLine());
+            LR = float.Parse(reader.ReadLine());
+            LR = float.Parse(reader.ReadLine());
+            hiddenVal = new float[numHiddens];
+            weightsIH = new float[numInputs, numHiddens];
+            for (int i = 0; i < numInputs; i++)
+                for (int h = 0; h < numHiddens; h++)
+                {
+                    weightsIH[i, h] = float.Parse(reader.ReadLine());
+                }
+            weightsHO = new float[numHiddens + 1, numOutputs];
+            for (int h = 0; h <= numHiddens; h++)
+                for (int o = 0; o < numOutputs; o++)
+                {
+                    weightsHO[h, o] = float.Parse(reader.ReadLine());
+                }
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+}
---
+++ RL.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO;
+
+public class RL : MonoBehaviour
+{
+    public struct Transition
+    {
+        public float[] state;
+        public int action;
+        public float reward;
+    };
+
+    NN neuralNetwork;
+
+    public Transform[] targets;
+    public Image[] images;
+    
+    public Text epsilonText;
+    float epsilon = 1;
+
+    int trainSteps = 0;
+
+    private void Start()
+    {
+        neuralNetwork = new NN(14, 10, 66);
+    }
+
+    public int Act(float[] state)
+    {
+        List<float> output = neuralNetwork.calcNet(state);
+        int maxIndex = 0;
+        float max = output[0], min = output[0];
+        for(int i = 1; i < output.Count; i++)
+        {
+            if(output[i] > max)
+            {
+                maxIndex = i;
+                max = output[i];
+            }
+            else if (output[i] < min)
+            {
+                min = output[i];
+            }
+        }
+        for (int i = 0; i < output.Count; i++)
+        {
+            float ratio = (output[i] - min) / (max - min);
+            images[i].color = new Color(1 - ratio, ratio, 0, 0.25f);
+        }
+        images[maxIndex].color = new Color(0, 1, 0, 0.5f);
+        return Random.Range(0.0f, 1.0f) >= epsilon ? maxIndex : Random.Range(0, targets.Length);
+    }
+
+    public void Observe(Transition newTransition)
+    {
+        neuralNetwork.Train(newTransition);
+        trainSteps++;
+        if(trainSteps % 100 == 0 && epsilon > 0.001f)
+        {
+            epsilon -= 0.01f;
+            epsilonText.text = "Epsilon = " + epsilon.ToString("0.00");
+        }
+    }
+
+    public void Save(StreamWriter writer)
+    {
+        writer.WriteLine(epsilon);
+        writer.WriteLine(trainSteps);
+        neuralNetwork.Save(writer);
+    }
+
+    public void Load(StreamReader reader)
+    {
+        try
+        {
+            epsilon = float.Parse(reader.ReadLine());
+            trainSteps = int.Parse(reader.ReadLine());
+            neuralNetwork.Load(reader);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        if (epsilon < 0.01f)
+            epsilon = 0;
+        epsilonText.text = "Epsilon = " + epsilon.ToString("0.00");
+    }
+}
